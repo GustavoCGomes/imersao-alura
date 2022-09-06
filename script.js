69448621a0f51b8813e7f01211b4dd9a3c5d9fad
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2022-09-05 21:11:01 -0300

    aula 001

---
+++ script.js
@@ -0,0 +1,21 @@
+var nome = 'Guilherme' // variaveis
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundooBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundooBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4 // operações matemáticas
+
+var notaFixada = notaFinal.toFixed(0) //to.Fixed casas decimais
+
+             //string
+console.log('Bem vindo ' + nome); //concatenação
+console.log('Sua media final é ' + notaFixada) //console.log
+
+//Isso é uma calculadora de medias//
+
+//Revisão//
+//Variáveis, strings, console.log, toFixed, operações matemática, concatenação
+//estilização do fundo
+
+
